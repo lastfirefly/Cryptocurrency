dd38623115d0e50330a7a5c10fbe4cce4070eb62
LuigiPereira1709 <luigipereira1001@gmail.com>
2024-03-04 15:45:00 -0300

    add arquive extract

--- scripts/extract/extract.py
+++ scripts/extract/extract.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-class ExtractArquive:
-    def __init__(self, arquiveName: str, urlDownload: str):
-        self.name = arquiveName
+class ExtrairArquivo:
+    def __init__(self, nomeArquivo: str, urlDownload: str):
+        self.nome = nomeArquivo
         self.url = urlDownload
 
     def ReadUrl(self):
-        fileName = pd.read_csv(f'{self.url}', sep=',')
-        return fileName
+        nomeArquivo = pd.read_csv(f'{self.url}', sep=',')
+        nomeArquivo = nomeArquivo.drop(columns=['SNo','Name','Volume'])
+        
+        return nomeArquivo
