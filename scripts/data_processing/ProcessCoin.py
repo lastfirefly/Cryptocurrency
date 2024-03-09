16e7458277091ef8490decaa83f41babdbdd4b85
Luigi Pereira <144620378+LuigiPereira1709@users.noreply.github.com>
2024-03-09 17:42:07 -0300

    feat: add functions settype and setdatecreation

--- scripts/data_processing/ProcessCoin.py
+++ scripts/data_processing/ProcessCoin.py
@@ -2,14 +2,37 @@ class ProcessCoin:
     def __init__(self, cryptoCoin: object):
         self.dfCoin = cryptoCoin
 
-    def Symbol(self):
-        return self.dfCoin['Symbol']
+    def getName(self):
+        return self.dfCoin['Name'].drop_duplicates()
 
-    def typeCoin(self):
-        pass
+    def setTypeCoin(self):
+        coin = self.dfCoin['Symbol'].drop_duplicates()
+        return coin.map({
+            "BNB": "cryptocurrency",
+            "BTC": "cryptocurrency",
+            "ADA": "cryptocurrency",
+            "DOGE": "cryptocurrency",
+            "ETH": "cryptocurrency",
+            "DOT": "cryptocurrency",
+            "USDT": "stablecoin",
+            "UNI": "cryptocurrency",
+            "USDC": "stablecoin",
+            "XRP": "cryptocurrency"
+        })
 
-    def creationDate(self):
-        pass
-
+    def setCreationDate(self):
+        coin = self.dfCoin['Symbol'].drop_duplicates()
+        return coin.map({
+            "BNB": "2017-07-25",
+            "BTC": "2009-01-03",
+            "ADA": "2017-09-29",
+            "DOGE": "2013-12-06",
+            "ETH": "2015-07-30",
+            "DOT": "2020-05-26",
+            "USDT": "2014-10-06",
+            "UNI": "2020-09-17",
+            "USDC": "2018-09-26",
+            "XRP": "2012-08-01"
+        })
 
 
