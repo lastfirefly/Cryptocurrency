a3b866b81e1d579007906cb30e9d27a38268c38b
LuigiPereira1709 <luigipereira1001@gmail.com>
2024-03-12 13:05:54 -0300

    fix: connection with db

--- scripts/load/LoadData.py
+++ scripts/load/LoadData.py
@@ -1,41 +1,35 @@
-import mysql.connector
+from database.Database import Database
 
-class LoadData():
+class LoadData:
     def __init__(self):
-        self.cnx = mysql.connector.connect(host="localhost", user="root", database="cryptocurrency")
-        self.cursor = self.cnx.cursor()
+        self.db = Database()
 
     def Load(self, *args: list) -> None:
-        closeConnection = False
-
-        if args and args[-1] == True:
-            closeConnection = True
-            args = args[:-1]
+        self.db.cursor.execute("USE cryptocurrency")
 
         for arg in args:
             table, dataFrame = arg
             for _, row in dataFrame.iterrows():
                 match table:
+                    case "valor_dia":
+                        sql = "INSERT INTO valor_dia (idmoedas_info, idmarketcap_dia, idabrefecha_dia, idminmax_dia, idcalendario)\
+                            VALUES (%s, %s, %s, %s, %s)"
+                        values = (int(row['idMoeda']), int(row['idValores']), int(row['idValores']), int(row['idValores']), int(row['idCalendario']))
                     case "moedas_info":
                         sql = "INSERT INTO moedas_info (nome, tipo_moeda, data_criacao) VALUES (%s, %s, %s)"
-                        values = (row['nome'], row['tipo_moeda'], row['data_criacao'])
+                        values = (str(row['nome']), str(row['tipo_moeda']), str(row['data_criacao']))
                     case "calendario":
-                        sql = "INSERT INTO calendario (datacompleta, diasemana, diames, mes, ano) VALUES (%s, %s, %s, %s, %s)"
-                        values = (row['data_completa'], row['diasemana'], row['diames'], row['mes'], row['ano'])
+                        sql = "INSERT INTO calendario (datacompleta, diasemana, diames, mes, ano, idmoedas_info) VALUES (%s, %s, %s, %s, %s, %s)"
+                        values = (str(row['data_completa']), str(row['diasemana']), str(row['diames']), str(row['mes']), str(row['ano']), int(row['id_moeda']))
                     case "marketcap_dia":
-                        sql = "INSERT INTO marketcap_dia (valor_marketcap) VALUES (%s)"
-                        values = (row['marketcap_dia'],)
+                        sql = "INSERT INTO marketcap_dia (valor_marketcap, idmoedas_info, idcalendario) VALUES (%s, %s, %s)"
+                        values = (float(row['marketcap_dia']), int(row['id_moeda']), int(row['idCalendario']))
                     case "abrefecha_dia":
-                        sql = "INSERT INTO abrefecha_dia (valor_abert, valor_fech) VALUES (%s, %s)"
-                        values = (row['valor_abre'], row['valor_fecha'])
+                        sql = "INSERT INTO abrefecha_dia (valor_abert, valor_fech, idmoedas_info, idcalendario) VALUES (%s, %s, %s, %s)"
+                        values = (float(row['valor_abre']), float(row['valor_fecha']), int(row['id_moeda']), int(row['idCalendario']))
                     case "minmax":
-                        sql = "INSERT INTO minmax_dia (max_valor, min_valor) VALUES (%s, %s)"
-                        values = (row['valor_max'], row['valor_max'])
+                        sql = "INSERT INTO minmax_dia (max_valor, min_valor, idmoedas_info, idcalendario) VALUES (%s, %s, %s, %s)"
+                        values = (float(row['valor_max']), float(row['valor_max']), int(row['id_moeda']), int(row['idCalendario']))
                 
-                self.cursor.execute(sql, values)
-
-        if closeConnection:
-            self.cnx.commit()
-            self.cursor.close()
-            self.cnx.close()
-            print('FOI CARALHO')
+                print(f"Inserting into {table} with values: {values}")
+                self.db.cursor.execute(sql, values)
